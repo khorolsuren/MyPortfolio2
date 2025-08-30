9a936615376702f17b2643bb3a135455ceebaa55
Khorolsuren Dashdavaa <khorolsuren1111@gmail.com>
2025-08-30 16:14:49 -0400

    change

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import path from "path"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/MyPortfolio2",
+  base: "/MyPortfolio",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src")
